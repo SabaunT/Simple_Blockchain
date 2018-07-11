1126d4a2583ee9e3e64a215201144109d279eedc
SabaunT <37265857+SabaunT@users.noreply.github.com>
2018-07-12 00:08:59 +0300

    Update merkle_tree.py

--- merkle_tree.py
+++ merkle_tree.py
@@ -1,14 +1,10 @@
 import hashlib
 
-#Предположим, это отхэшированный список транзакций
-Tx_hashlist = ['A100', 'B200', 'C300', 'D400',
-			   'E500', 'F600', 'G700', 'H800'
-			  ]
+
 #Структура в которой будут храниться отхэшированные транзакции
 merkle_tree_data = []
 
-
-def merkle_tree(haslist):
+def merkle_tree(hashlist): #hashlist является списком прохэшированных дважды транзакций по-отдельности
 	global merkle_tree_data
 	#когда останется последний элемент, он просто станет корнем, это нерекурентный случай
 	if len(merkle_tree_data) == 14:
@@ -21,11 +17,14 @@ def merkle_tree(haslist):
 	#такое разделение обусловлено тем, что не стали для определения диапазона генерации использовать логарифмы
 	#либо я даун и не смог нормально написать общий range и для случая, когда остается элемента 2 дабл-хэша
 	if len(hashlist) == 2:
-		additional_arg = hashlib.sha256(hashlist[0]+hashlist[1])
+		additional_arg = hashlib.sha256(hashlist[0]+hashlist[1]).hexdigest()
 		new_hash_list.append(additional_arg)
 	else:
 		for y in range(0, len(hashlist)-1, 2):
-			additional_arg = hashlib.sha256(hashlist[y]+hashlist[y+1])
+			additional_arg = hashlib.sha256(hashlist[y]+hashlist[y+1]).hexdigest()
 			new_hash_list.append(additional_arg)
 	merkle_tree(new_hash_list)
+
+merkle_tree(Tx_hashlist)
+print (merkle_tree_data)
 
