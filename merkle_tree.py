d95f64662d6d9062b8f8bb36326fa75bf9f74f2e
SabaunT <37265857+SabaunT@users.noreply.github.com>
2018-07-12 19:17:18 +0300

    Update merkle_tree.py

--- merkle_tree.py
+++ merkle_tree.py
@@ -1,16 +1,20 @@
 import hashlib
 
-
+#list of transactions example
 Tx_hashlist1 = ['A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8']
+#hash of each transaction is added to the list below; the lowest leaf is based
 Tx_hashlist = [hashlib.sha256(each.encode()).hexdigest() for each in Tx_hashlist1]
 
+#merkle tree presented as an array (list in python)
 merkle_tree_data = []
 
 def merkle_tree(hashlist):
 	global merkle_tree_data
+	#non-recursion case
 	if len(merkle_tree_data) == 14:
 		merkle_tree_data.insert(0, hashlist[0])
 		return merkle_tree_data
+	#recursion case
 	for x in range(len(hashlist)-1, -1, -1):
 		merkle_tree_data.insert(0, hashlist[x])
 	new_hash_list = []
